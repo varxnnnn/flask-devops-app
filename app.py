813c48d7f7bbcc067501f4a562dbc67288756ad0
varxnnnn <varunkumar615768@gmail.com>
2025-06-07 13:58:32 +0530

    d

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Iam a VARUN KUMAR,I am intrested in Software devlopment"
+    return "Hello, Iam a VARUN KUMAR"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
